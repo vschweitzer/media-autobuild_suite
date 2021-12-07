15ad8fd1233ce93450309bb233143ecaf86ec066
vschweitzer <valentin.schweitzer98@gmail.com>
2021-12-07 17:59:26 +0100

    Add dependency version dump

--- build/media-suite_helper.sh
+++ build/media-suite_helper.sh
@@ -251,6 +251,9 @@ do_vcs() {
     # if so, set ref to the ref on the origin, this might make it harder for people who
     # want use multiple remotes other than origin. Converts ref=develop to ref=origin/develop
     # ignore those that use the special tags/branches
+
+    save_version_info "$vcsURL" "$ref"
+
     case $ref in
     LATEST | GREATEST | *\**) ;;
     *) git ls-remote --exit-code "$vcsURL" "${ref#origin/}" > /dev/null 2>&1 && ref=origin/${ref#origin/} ;;
@@ -2550,4 +2553,30 @@ safe_git_clean() {
         -e '/custom_updated' \
         -e '**/ab-suite.*.log' \
         "${@}"
+}
+
+save_version_info() {
+    dependency_url=${1}
+    dependency_ref=${2}
+    dependency_commit=""
+
+    if [ -z ${1+x} ]; then
+        rm -f ./dependency_versions.txt
+        return
+    fi
+
+    case $dependency_ref in
+    LATEST | GREATEST | *\**) dependency_commit=$(git ls-remote $dependency_url HEAD)
+    echo "LATEST/GREATEST" ;;
+    origin/*) dependency_commit=$(git ls-remote $dependency_url ${dependency_ref#origin/})
+    echo "origin/*";;
+    *) git ls-remote --exit-code "$dependency_url" "${dependency_ref#origin/}" > /dev/null 2>&1 && dependency_ref=origin/${dependency_ref#origin/}
+    dependency_commit=$(git ls-remote $dependency_url ${dependency_ref#origin/})
+    echo "Default" ;;
+    esac
+
+    dependency_commit=${dependency_commit:0:40}
+
+    echo "${dependency_url};${dependency_ref};${dependency_commit}"
+    echo "${dependency_url};${dependency_ref};${dependency_commit}" >> ./dependency_versions.txt
 }
